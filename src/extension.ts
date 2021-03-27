9dd42c729bef7a660b9ebc9989dbd5cee8223333
Balazs Fodor <fodorbalazs@freemail.hu>
2021-03-27 08:25:56 +0100

    Generate icon and use it

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { InputBoxOptions } from 'vscode';
+import { InputBoxOptions, Uri } from 'vscode';
 
 import * as path from 'path';
 
 import { QuickPickItem } from 'vscode';
+import { fstat } from 'node:fs';
 
 class PickItem implements QuickPickItem {
 	label: string;
@@ -40,16 +41,16 @@ export function activate(context: vscode.ExtensionContext) {
 		// vscode.window.showInformationMessage(path.join(__dirname, '..', 'resources', 'light', 'asd.svg'));
 
 		// set a decoration on some line
-		let deco = vscode.window.createTextEditorDecorationType(
-			{
-				gutterIconPath: path.join(__dirname, '..', 'resources', 'bmffff66.svg'),
-				gutterIconSize: 'contain',
-			}
-		);
-		let range1 = new vscode.Range(1, 0, 1, 0);
-		let range2 = new vscode.Range(2, 0, 3, 0);
-		let editor = vscode.window.activeTextEditor;
-		editor?.setDecorations(deco, [range1, range2]);
+		// let deco = vscode.window.createTextEditorDecorationType(
+		// 	{
+		// 		gutterIconPath: path.join(__dirname, '..', 'resources', 'bmffff66.svg'),
+		// 		gutterIconSize: 'contain',
+		// 	}
+		// );
+		// let range1 = new vscode.Range(1, 0, 1, 0);
+		// let range2 = new vscode.Range(2, 0, 3, 0);
+		// let editor = vscode.window.activeTextEditor;
+		// editor?.setDecorations(deco, [range1, range2]);
 		// Available values are 'auto', 'contain', 'cover' and any percentage
 
 		// show quick pick
@@ -95,6 +96,56 @@ export function activate(context: vscode.ExtensionContext) {
 		// 	editor2.selection = new vscode.Selection(range.start, range.start);
 		// 	editor2.revealRange(range);
 		// }
+
+		// stat non existing / not existing file
+		// try {
+		// 	let stat = await vscode.workspace.fs.stat(Uri.file(path.join(__dirname, '..', 'resources', 'bmffff66.svg')));
+		// 	vscode.window.showInformationMessage("Stat 1 size: " + stat.size + " mtime: " + stat.mtime + " type: " + stat.type);
+		// 	let stat2 = await vscode.workspace.fs.stat(Uri.file(path.join(__dirname, '..', 'resources', 'bm.svg')));
+		// 	vscode.window.showInformationMessage("Stat 2 size: " + stat2.size + " mtime: " + stat2.mtime + " type: " + stat2.type);
+		// } catch (e) {
+		// 	vscode.window.showInformationMessage("stat exception");
+		// }
+
+		// writing an svg
+		let svgDir = Uri.joinPath(context.globalStorageUri, "svg");
+		try {
+			await vscode.workspace.fs.createDirectory(svgDir);
+			vscode.window.showInformationMessage(svgDir.path + " created");
+		} catch (e) {
+			vscode.window.showInformationMessage("dir " + svgDir.path + " exception");
+		}
+
+		let svgSource = new Uint8Array([0x3c, 0x73, 0x76, 0x67, 0x20, 0x78, 0x6d, 0x6c, 0x6e, 0x73, 0x3d, 0x22, 0x68, 0x74, 0x74, 0x70, 0x3a, 0x2f, 0x2f, 0x77, 0x77, 0x77, 0x2e, 0x77, 0x33, 0x2e, 0x6f, 0x72, 0x67, 0x2f, 0x32, 0x30, 0x30, 0x30, 0x2f, 0x73, 0x76, 0x67, 0x22, 0x20, 0x77, 0x69, 0x64, 0x74, 0x68, 0x3d, 0x22, 0x33, 0x32, 0x22, 0x20, 0x68, 0x65, 0x69, 0x67, 0x68, 0x74, 0x3d, 0x22, 0x33, 0x32, 0x22, 0x3e, 0x3c, 0x70, 0x61, 0x74, 0x68, 0x20, 0x64, 0x3d, 0x22, 0x4d, 0x37, 0x20, 0x33, 0x30, 0x20, 0x4c, 0x37, 0x20, 0x38, 0x20, 0x51, 0x37, 0x20, 0x32, 0x20, 0x31, 0x33, 0x20, 0x32, 0x20, 0x4c, 0x31, 0x39, 0x20, 0x32, 0x20, 0x51, 0x32, 0x35, 0x20, 0x32, 0x20, 0x32, 0x35, 0x20, 0x38, 0x20, 0x4c, 0x32, 0x35, 0x20, 0x33, 0x30, 0x20, 0x4c, 0x31, 0x36, 0x20, 0x32, 0x33, 0x20, 0x5a, 0x22, 0x20, 0x66, 0x69, 0x6c, 0x6c, 0x3d, 0x22, 0x23, 0x66, 0x66, 0x66, 0x66, 0x36, 0x36, 0x22, 0x20, 0x2f, 0x3e, 0x3c, 0x2f, 0x73, 0x76, 0x67, 0x3e]);
+		//replace color part
+		for (let i = 134; i < 140; i++) {
+			svgSource[i] = 0x65;
+		}
+		let svgUri = Uri.joinPath(svgDir, "test.svg");
+		try {
+			await vscode.workspace.fs.writeFile(svgUri, svgSource);
+			vscode.window.showInformationMessage(svgUri.path + " written");
+		} catch (e) {
+			vscode.window.showInformationMessage("dir " + svgDir.path + " exception");
+		}
+		try {
+			let stat = await vscode.workspace.fs.stat(svgUri);
+			vscode.window.showInformationMessage("Stat size: " + stat.size + " mtime: " + stat.mtime + " type: " + stat.type);
+		} catch (e) {
+			vscode.window.showInformationMessage("stat exception");
+		}
+
+		// use the new icon for gutter decoration
+		let deco = vscode.window.createTextEditorDecorationType(
+			{
+				gutterIconPath: svgUri,
+				gutterIconSize: 'contain',
+			}
+		);
+		let range1 = new vscode.Range(1, 0, 1, 0);
+		let range2 = new vscode.Range(2, 0, 3, 0);
+		let editor = vscode.window.activeTextEditor;
+		editor?.setDecorations(deco, [range1, range2]);
 
 		context.subscriptions.push(disposable);
 	});
